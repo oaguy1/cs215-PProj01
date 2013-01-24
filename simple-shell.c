174e3c4b68de9b7eb589f69ac0d36c7d187e81fb
David N. Robinson <oaguy1@gmail.com>
2013-01-24 14:33:29 -0500

    Started accepting commands, very rocky

--- simple-shell.c
+++ simple-shell.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 
 
@@ -14,15 +15,29 @@
 
 int main(void)
 {
-	char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
+    char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
+    char cmd[20];      //temp variable until we can properly deal with args
     int should_run = 1;
-	
-	int i, upper;
-		
+
+    int i, upper;
+    pid_t pid;
+
     while (should_run){   
         printf("osh>");
         fflush(stdout);
-        
+        scanf("%s", cmd);
+        pid = fork();
+
+        if (pid < 0) { //error occured
+            fprintf(stderr, "Fork failed\n");
+            return 1;
+        } else if (pid == 0) {
+            execvp(cmd, args);
+        } else {
+            printf("I am the parent %d\n",pid);
+	    wait(NULL);
+        }//while
+
         /**
          * After reading user input, the steps are:
          * (1) fork a child process
@@ -30,6 +45,6 @@ int main(void)
          * (3) if command included &, parent will invoke wait()
          */
     }
-    
-	return 0;
+
+    return 0;
 }
