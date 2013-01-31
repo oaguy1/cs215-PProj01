2e758c1a30a82cf1c11c0b8d178431843680ca67
David N. Robinson <oaguy1@gmail.com>
2013-01-31 15:05:03 -0500

    Added string parsing and exit function, broken-ish

--- simple-shell.c
+++ simple-shell.c
@@ -16,31 +16,49 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <string.h>
 
-
-
-#define MAX_LINE		80 /* 80 chars per line, per command */
+#define MAX_LINE		80  /* 80 chars per line, per command */
 
 int main(void)
 {
-    char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
-    char cmd[20];      //temp variable until we can properly deal with args
+    char input[MAX_LINE];
+    char cmd[MAX_LINE];
+    char args[MAX_LINE];
+    char temp[MAX_LINE];
     int should_run = 1;
 
     int i, upper;
     pid_t pid;
 
     while (should_run){   
-        printf("osh>");
+        printf("jdcsh>");
         fflush(stdout);
-        scanf("%s", cmd);
+        scanf("%s", input);
         pid = fork();
 
+        strcpy(cmd, strtok(input, " "));
+        printf("%s\n", input);
+        i = 0;
+        
+        // parse command into tokens
+        /*
+        do {
+            strcpy(strtok(NULL, " "), temp);
+            strcat(args, temp);
+            printf("%s\n", args);
+        } while (temp != NULL);
+        */
+
+        // evaluate command
         if (pid < 0) { //error occured
             fprintf(stderr, "Fork failed\n");
             return 1;
+        } else if(!strcmp(cmd, "exit")) {
+            printf("You have failed me for the last time, commander!\n");
+            return 0;
         } else if (pid == 0) {
-            execvp(cmd, args);
+            execvp(cmd, *args);
         } else {
             printf("I am the parent %d\n",pid);
 	    wait(NULL);
