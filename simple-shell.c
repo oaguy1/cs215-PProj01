69ee839c6dceeee8461a19f78692447e1bc10892
Jasper Boyd <jboyd@clarku.edu>
2013-01-31 22:08:22 -0500

    Simple-Shell is executiing commands
    Need to fix the fact that it only accepts one command

--- simple-shell.c
+++ simple-shell.c
@@ -1,17 +1,10 @@
 /**
- * Simple shell interface program.
- *
- * Operating System Concepts - Ninth Edition
- * Copyright John Wiley & Sons - 2013
+ * The steps are:
+ * (1) parse the input to obtain the command and parameters
+ * (2) invoke execvp() to start the process
+ * (3) Obtain the output stream
+ * (4) Output the contents returned by the command
  */
-
- /**
-  * The steps are:                          
-  * (1) parse the input to obtain the command and parameters
-  * (2) invoke execvp() to start the process
-  * (3) Obtain the output stream                       
-  * (4) Output the contents returned by the command 
-  */ 
 
 #include <stdio.h>
 #include <unistd.h>
@@ -22,48 +15,50 @@
 
 int main(void)
 {
-    char input[MAX_LINE];
-    char cmd[MAX_LINE];
-    char args[MAX_LINE];
-    char temp[MAX_LINE];
+    char *temp = NULL;
     int should_run = 1;
-
-    int i, upper;
     pid_t pid;
-
-    while (should_run){   
+    char *cmd[MAX_LINE];
+	char input[MAX_LINE];
+    
+    while (should_run){
         printf("jdcsh>");
         fflush(stdout);
-        scanf("%s", input);
-        pid = fork();
-
-        strcpy(cmd, strtok(input, " "));
-        printf("%s\n", input);
-        i = 0;
         
-        // parse command into tokens
-        /*
-        do {
-            strcpy(strtok(NULL, " "), temp);
-            strcat(args, temp);
-            printf("%s\n", args);
-        } while (temp != NULL);
-        */
-
+        int num_args = 0;
+        fgets(input, MAX_LINE, stdin);
+        printf("\n"); //new line
+        
+        //Parse the input into cmd and argumentsi
+        
+        cmd[0] = strtok(input, " ");
+        
+        temp = strtok(NULL, " ");
+        
+        while (temp != NULL) {
+            num_args++;
+            cmd[num_args] = temp;
+            temp = strtok(NULL, " ");
+        }
+        
+        //remove the \n that gets added to the end
+        int lcmd_len = (int) strlen(cmd[num_args]);
+        cmd[num_args][lcmd_len-1] = '\0';
+        
+        pid = fork();
+        
         // evaluate command
-        if (pid < 0) { //error occured
-            fprintf(stderr, "Fork failed\n");
-            return 1;
-        } else if(!strcmp(cmd, "exit")) {
+        if (pid < 0) {
+            fprintf(stderr, "Your fork has failed you.\n");
+            return 1; 
+        } else if(!strcmp(cmd[0], "exit")) {
             printf("You have failed me for the last time, commander!\n");
-            return 0;
-        } else if (pid == 0) {
-            execvp(cmd, *args);
+            exit(0); 
         } else {
-            printf("I am the parent %d\n",pid);
-	    wait(NULL);
+            //broken here we need to put in the file in argument 1.
+            execvp(cmd[0], cmd);
         }
     }//while
-
+    
     return 0;
 }
