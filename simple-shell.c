436f4c778ae90587bd13f5d0cded6dde20ebc278
Jasper Boyd <jboyd@clarku.edu>
2013-02-06 13:45:12 -0500

    Revert "I don't what I did"
    This reverts commit b78a0c15bc0cfc0cba366d64bc3f0bc3971108a9.

--- simple-shell.c
+++ simple-shell.c
@@ -13,67 +13,73 @@
 
 #define MAX_LINE		80  /* 80 chars per line, per command */
 
-void execute(char** argv){
-    pid_t pid;
-    int status;
-    pid = fork();
-    
-    if (pid < 0){
-        printf("Your fork has failed you\n");
-        exit(1);
+void execute(char** argv){ 
+    pid_t pid;  
+    int status; 
+    pid = fork(); 
+
+    if (pid < 0){ 
+        printf("Your fork has failed you\n"); 
+        exit(1); 
     } else if (pid == 0){
         if(execvp(argv[0], argv) < 0){
             printf("Your child has failed you\n");
-            exit(1);
+            exit(1); 	
         }
-    } else if(!strcmp(argv[0], "exit") || !strcmp(argv[0], "quit")){
-        printf("You have failed me for the last time commander\n");
-        exit(0);
+    } else if(!strcmp(argv[0], "exit") || !strcmp(argv[0], "quit")){ 
+        printf("You have failed me for the last time commander\n"); 
+        exit(0); 
     } else if(!strcmp(argv[0], "cd")) {
-        chdir(argv[1]);
-    } else {
+            chdir(argv[1]);
+    } else { 
         while(wait(&status) != pid)
             ;// do nothing
     }
-    
-}
+
+} 
 
 int main(void)
 {
-    char *cwd = NULL;
+    char *cwd[FILENAME_MAX];
     char *temp = NULL;
     int should_run = 1;
-    
+
     char *cmd[MAX_LINE];
     char input[MAX_LINE];
-    
+
     while (should_run){
         // grab current working directory and print prompt
         cwd = getwd(cwd);
         printf("jdcsh: %s>", cwd);
         fflush(stdout);
-        
+
         int num_args = 0;
         fgets(input, MAX_LINE, stdin);
         printf("\n"); //new line
-        
+
         //Parse the input into cmd and arguments
         cmd[0] = strtok(input, " ");
         temp = strtok(NULL, " ");
-        
+
         while (temp != NULL) {
             num_args++;
             cmd[num_args] = temp;
             temp = strtok(NULL, " ");
         }//while
-        
+
         //remove the \n that gets added to the end
         int lcmd_len = (int) strlen(cmd[num_args]);
         cmd[num_args][lcmd_len-1] = '\0';
+<<<<<<< HEAD
         
+=======
+
+
+
+>>>>>>> Revert "I don't what I did"
         execute(cmd); 
     }//while
-    
+
     return 0;
 }
 
