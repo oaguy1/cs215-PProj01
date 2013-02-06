6d833cb3642a90f77354bbb691b6df18fb7f7740
Jasper Boyd <jboyd@clarku.edu>
2013-02-06 13:53:00 -0500

    Revert "more junk"
    This reverts commit bfc77786494be80e0ae800f6a55bcded6cd1bc01.

--- simple-shell.c
+++ simple-shell.c
@@ -40,7 +40,7 @@ void execute(char** argv){
 
 int main(void)
 {
-    char *cwd = NULL;
+    char *cwd[FILENAME_MAX];
     char *temp = NULL;
     int should_run = 1;
 
@@ -49,7 +49,11 @@ int main(void)
 
     while (should_run){
         // grab current working directory and print prompt
+<<<<<<< HEAD
         cwd = getwd(cwd); 
+=======
+        cwd = getwd(cwd);
+>>>>>>> Revert "more junk"
         printf("jdcsh: %s>", cwd);
         fflush(stdout);
 
