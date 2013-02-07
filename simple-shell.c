beb4e6b4788f3204a68dfbc88e625b70bd881203
Jasper Boyd <jboyd@clarku.edu>
2013-02-07 09:10:22 -0500

    Revert "Revert "Revert "junk"""
    This reverts commit 027fed33301e389befa98328950b14b2f7f048ab.

--- simple-shell.c
+++ simple-shell.c
@@ -12,18 +12,12 @@
 #include <string.h>
 
 #define MAX_LINE		80  /* 80 chars per line, per command */
-#define MAX_SIZE		100 //number of cmds held in history.
-
-//init history
-char history[MAX_SIZE][MAX_LINE]; 
-int history_count = 0;  
 
 void execute(char** argv){ 
     pid_t pid;  
     int status; 
     pid = fork(); 
 
-<<<<<<< HEAD
     if (pid < 0){ 
         printf("Your fork has failed you\n"); 
         exit(1); 
@@ -42,46 +36,6 @@ void execute(char** argv){
             ;// do nothing
     }
 
-=======
-   if (pid < 0){ 
-   	printf("Your fork has failed you\n"); 
-        exit(1); 
-   } else if (pid == 0){
-   	if(execvp(argv[0], argv) < 0){
-        	printf("Your child has failed you\n");
-	       exit(1); 	
-	}
-   } else { 
-   	while(wait(&status) != pid)
-		;
-   } 
-    
-}
-
-char* cmd_num(int num){ 
-     //need some error handling
-     return history[num];
-} 
-
-char* last_cmd(){ 
-     return history[0]; 
-} 
-
-void put_in_history(char** a_cmd){ 
-     if (history_count == 0){ 
-     	strcpy(history[history_count++], a_cmd); 
-     }  else if (history_count + 1 == MAX_SIZE){ 
-     	//everything needs to be moved up
-	int temp = history_count + 1; 
-	while (temp != 0){	
-		strcpy(history[temp--], history[temp]); 
-	}
-	//temp = 0;
-	strcpy(history[temp], a_cmd); 
-     }  else { 
-     	fprintf(stderr, "Error: Array out of bounds)"); 
-     }
->>>>>>> junk
 } 
 
 int main(void)
@@ -93,11 +47,6 @@ int main(void)
     char *cmd[MAX_LINE];
     char input[MAX_LINE];
 
-<<<<<<< HEAD
-=======
-   
-
->>>>>>> junk
     while (should_run){
         // grab current working directory and print prompt
 <<<<<<< HEAD
@@ -120,7 +69,6 @@ int main(void)
             num_args++;
             cmd[num_args] = temp;
             temp = strtok(NULL, " ");
-<<<<<<< HEAD
         }//while
 
         //remove the \n that gets added to the end
@@ -129,36 +77,9 @@ int main(void)
         
         execute(cmd);
 
-<<<<<<< HEAD
         // flush *cmd so that it is completely empty,
         // prevents residual commands being passed
         memset(cmd, 0, sizeof(cmd));
-=======
->>>>>>> Revert "I don't what I did"
-        execute(cmd); 
-=======
-        }
-        
-        //remove the \n 
-	//that gets added to the end
-        int lcmd_len = (int) strlen(cmd[num_args]);
-        cmd[num_args][lcmd_len-1] = '\0';
-    	
-        if(!strcmp(cmd[0], "exit") || !strcmp(cmd[0], "quit")){ 
-        	printf("You have failed me for the last time commander\n"); 
-		exit(0); 
-	} else if (!strcmp(cmd[0], "!!")) { 
-		execute(last_cmd()); 
-		put_in_history(cmd); 
-	} else if (!strcmp(cmd[0][0], "!")){ 
-	        execute(cmd_num(cmd[0][1]));
-		put_in_history(cmd); 	
-	} else { 
-		execute (cmd); 
-		put_in_history(cmd);
-	}	
->>>>>>> junk
->>>>>>> Revert "Revert "junk""
     }//while
 
     return 0;
